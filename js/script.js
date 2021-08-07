6ee3ed0b4ba5c1e216d73d295904688a23b86fa2
Daniel <dan767@yandex.ru>
2021-08-07 22:59:39 +0700

    json server для post и get запросов

--- js/script.js
+++ js/script.js
@@ -97,10 +97,23 @@ const message = {
 };
 
 forms.forEach(form => {
-    postData(form);
+    bindPostData(form);
 });
 
-function postData(form) {
+const postData = async (url, data) => {
+    //асинхронный код
+    const res = await fetch(url, {
+        method: "POST",
+        headers: {'Content-type': 'application/json'},
+        body: data
+    });
+
+    return await res.json();
+};
+
+
+
+function bindPostData(form) {
     form.addEventListener('submit', (e) => {
         e.preventDefault(); //Во всех ajax запросов чтоб не перезагружалась стр
         //Динамически создаваемый блок к форме
@@ -113,16 +126,11 @@ function postData(form) {
         form.insertAdjacentElement('afterend', statusMessage);
 
         const formData = new FormData(form);
-        const obj = {};
-        formData.forEach(function(value, key) {
-            obj[key] = value;
-        });
 
-        fetch('server.php', { //Для фетча http ошибки не являются rejectом
-            method: "POST",
-            headers: {'Content-type': 'application/json'},
-            body: JSON.stringify(obj)
-        }).then(data => data.text())
+        const json = JSON.stringify(Object.fromEntries(formData.entries())); //новые методы
+
+
+        postData('http://localhost:3000/requests', json)
         .then(data => {
             console.log(data);
             showThanksModal(message.success);
@@ -201,36 +209,48 @@ class MenuCard {
 
 }
 
-//const div = new MenuCard(...);
-//div.render(); или 
-new MenuCard(
-    'img/tabs/vegy.jpg',
-    'vegy',
-    'Меню "Фитнес"',
-    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-    229,
-    '.menu .container',
-    'menu__item'
-    
-).render(); //тут же удалится
-new MenuCard(
-    'img/tabs/elite.jpg',
-    'elite',
-    'Меню “Премиум”',
-    'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-    550,
-    '.menu .container',
-    'menu__item'
-).render();
-new MenuCard(
-    'img/tabs/post.jpg',
-    'post',
-    'Меню "Постное"',
-    'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-    430,
-    '.menu .container',
-    'menu__item'
-).render();
+const getReource = async (url) => {
+    //асинхронный код
+    const res = await fetch(url);
+    if (!res.ok) {
+        throw new Error(`Ошибка запроса к ${url}, статус: ${res.status}`)
+    }
+
+    return await res.json();
+};
+
+// getReource('http://localhost:3000/menu').then(data => {
+//     data.forEach(({img, altimg, title, descr, price}) => { //деструктуризация объекта
+//         new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+//     });
+// });
+
+
+//getReource('http://localhost:3000/menu').then(data => createCard(data));
+// function createCard(data) {
+//     data.forEach(({img, altimg, title, descr, price}) => {
+//         const element = document.createElement('div');
+//         element.classList.add('menu__item');
+//         element.innerHTML = `
+//         <img src="${img}" alt="${altimg}">
+//         <h3 class="menu__item-subtitle">${title}</h3>
+//         <div class="menu__item-descr">${descr}</div>
+//         <div class="menu__item-divider"></div>
+//         <div class="menu__item-price">
+//             <div class="menu__item-cost">Цена:</div>
+//             <div class="menu__item-total"><span>${price*73}</span> р/день</div> 
+//     `;// цена в долларах, можно подгружать
+
+//     document.querySelector('.menu .container').append(element);
+//     });
+// }
+
+axios.get('http://localhost:3000/menu').then(data => {
+
+    data.data.forEach(({img, altimg, title, descr, price}) => { //деструктуризация объекта
+        new MenuCard(img, altimg, title, descr, price * 74, '.menu .container').render();});
+
+});
 
 //Таймер
 const deadline = '2022-01-01';
