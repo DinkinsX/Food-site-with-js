bf9ee7c03e9dbc69298aba6e3c10b53cbdb96cce
Daniel <dan767@yandex.ru>
2021-08-07 11:12:58 +0700

    realize timer

--- js/script.js
+++ js/script.js
@@ -159,3 +159,52 @@ new MenuCard(
     '.menu .container',
     'menu__item'
 ).render();
+
+//Таймер
+const deadline = '2022-01-01';
+function getTimeRemaining(endtime) {
+    const time = Date.parse(endtime) - Date.parse(new Date()),
+        days = Math.floor(time / (1000 * 60 * 60 * 24)),
+        hours = Math.floor((time / (1000 * 60 * 60) % 24)),
+        minutes = Math.floor((time / 1000 / 60) % 60),
+        seconds = Math.floor((time / 1000) % 60);
+    
+    return {
+        'total': time,
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
+    };
+}
+function getZero(num) {
+    if (num >= 0 && num < 10) {
+        return `0${num}`;
+    } else {
+        return `${num}`;
+    }
+}
+function setClock(selector, endtime) {
+    const timer = document.querySelector(selector),
+        days = timer.querySelector('#days'),
+        hours = timer.querySelector('#hours'),
+        minutes = timer.querySelector('#minutes'),
+        seconds = timer.querySelector('#seconds'),
+        timeInterval = setInterval(updateClock, 1000);
+
+    updateClock();
+    function updateClock() {
+        const times = getTimeRemaining(endtime);
+        days.innerHTML = getZero(times.days);
+        hours.innerHTML = getZero(times.hours);
+        minutes.innerHTML = getZero(times.minutes);
+        seconds.innerHTML = getZero(times.seconds);
+
+        if (times.total <= 0) {
+            clearInterval(timeInterval);
+        }
+    }
+
+}
+
+setClock('.timer', deadline);
